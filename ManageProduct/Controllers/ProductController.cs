a0fa13e43b1a2efbc8aa79dadd8986dfaaeafc1d
mohamad amin rashidi nejad <mohammadAminRashidiNeJad@gmail.com>
2025-10-30 12:53:41 +0330

    Delete

--- ManageProduct/Controllers/ProductController.cs
+++ ManageProduct/Controllers/ProductController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ManageProduct.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ManageProduct.Controllers
 {
     public class ProductController : Controller
     {
+        ProductRepository repository=new ProductRepository();
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var item=repository.Products.FirstOrDefault(p=>p.Id==id);
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var item = repository.Products.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                item.IsExist = false;
+            }
+            return RedirectToAction("Index");
         }
     }
 }
