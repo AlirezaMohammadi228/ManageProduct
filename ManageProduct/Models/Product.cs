9729f25b93f7591673ac3cd9b8effb52edf4a2ce
TheSamer <TheSamer@DESKTOP-EUEOQPQ>
2025-10-30 10:14:05 +0330

    Add project files.

---
+++ ManageProduct/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace ManageProduct.Models
+{
+    public enum ProductType
+    {
+        None = 0,
+        Clothes = 1,
+        Detergent = 2,
+        Food = 3,
+    }
+    public class Product
+    {
+        
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public ProductType productType { get; set; }
+        public int Quantity { get; set; }
+        public int Id { get; set; }
+        public bool IsExist { get; set; }
+    }
+}
---
+++ ManageProduct/Models/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace ManageProduct.Models
+{
+    public class ProductRepository
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public decimal TotalValue()
+        {
+            return Products.Sum(x => x.Quantity * x.Price);
+        }
+        public void Add(Product product)
+        {
+            Products.Add(product);
+        }
+        public void Delete(Product product)
+        {
+            Products.Remove(product);
+        }
+        public void Edit(Product product)
+        {
+           
+        }
+        public List<Product> GetAll()
+        {
+            return Products;
+        }
+        public Product GetById(int id)
+        {
+            return Products.FirstOrDefault( x => x.Id == id);
+        }
+    }
+}
