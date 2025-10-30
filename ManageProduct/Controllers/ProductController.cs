e2b776b3b3021313ebe05e18cc31fbf40c26111c
ehsan_arefzadeh <arefzaadeh@gmail.com>
2025-10-30 12:50:24 +0330

    View

--- ManageProduct/Controllers/ProductController.cs
+++ ManageProduct/Controllers/ProductController.cs
@@ -9,21 +9,32 @@ namespace ManageProduct.Controllers
         public static ProductRepository Repository { get; set; } = new();
         public IActionResult Index()
         {
-            return View();
+            return View(Repository.GetAll());
         }
-        public IActionResult Update()
+        public IActionResult Create()
         {
-            //var model = Repository.GetById(id);
-            //return View(model);
             return View();
         }
-        //public IActionResult Update(Product model)
-        //{
-        //    var preModel = Repository.GetById(model.Id);
-        //    preModel.Name = model.Name;
-        //    preModel.Price = model.Price;
-        //    preModel.productType = model.productType;
-        //    return RedirectToAction("Index");
-        //}
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            Repository.Add(product);
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Update(int id)
+        {
+            var model = Repository.GetById(id);
+            return View(model);
+
+        }
+        public IActionResult Update(Product model)
+        {
+            var preModel = Repository.GetById(model.Id);
+            preModel.Name = model.Name;
+            preModel.Price = model.Price;
+            preModel.productType = model.productType;
+            return RedirectToAction("Index");
+        }
     }
 }
